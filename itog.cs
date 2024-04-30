bdd137a9fa30717926dfc99349bac1109bfafdf7
valentin <eraldinir@gmail.com>
2024-04-30 16:46:47 +0300

    semi-working srcript

--- itog.cs
+++ itog.cs
@@ -1,1 +1,18 @@
-пшеgit
+using static System.Console;
+WriteLine("Введите данные через пробел, затем нажмите Enter: ");
+
+string? input = ReadLine();
+string[] all = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+foreach (var item in all)
+{
+    Write(item);
+}
+
+foreach (var item in all)
+{
+    if (item.Length <= 3)
+        {
+            Write(item);
+        }
+}
